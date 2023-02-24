f111759c25a78e04787c8cf443222c42645ad01a
Ferbray <yt_ferbray@mail.ru>
2023-02-24 17:20:55 +0300

    understand v2

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const jsonParser = express.json();
 
 app.use(express.static(__dirname + "/wwwroot"));
 
+app.get("/email/confirm:userId&emailToken", function(request, response) {
+	const userId = request.params["userId"];
+	const emailToken = request.params["emailToken"];
+	const userIp = request.query.ip;
+	const userPlatform = request.query.platform;
+	response.sendFile(__dirname + "/view/email/confirm.html");
+});
+
 app.get("/", function(request, response) {
 	response.sendFile(__dirname + "/index.html");
 });
---
+++ wwwroot/js/change-email.js
@@ -0,0 +1,2 @@
+let newEmail = prompt('Ваша новая почта', "");
+
