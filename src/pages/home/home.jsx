b910c9415fa0a27a0955327d9b3ec1240ca4223e
zmqp <domarlaid2@mail.ru>
2023-05-01 00:13:18 +0500

    add banner template

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Helmet} from "react-helmet";
 import ItemRoulette from "../../components/item/item-roulette";
+import Banner from "../../components/banner/banner";
 
 class Home extends React.Component {
     title = "InCase - ";
@@ -17,6 +18,7 @@ class Home extends React.Component {
                     <title>{this.title}</title>
                 </Helmet>
                 <ItemRoulette/>
+                <Banner/>
                 <h1>Main</h1>
                 <h1>Main</h1>
                 <h1>Main</h1>
