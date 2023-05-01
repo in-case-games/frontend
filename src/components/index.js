3ace03f8d1d8bb946c9f58b2e92a516453ea42c1
Ferbray <yt_ferbray@mail.ru>
2023-05-01 13:16:36 +0300

    доделать

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { ItemRoulette } from "./item";
 export { default as BoxGroup } from "./box-group/box-group";
 export { default as Banner } from "./banner/banner";
+export { default as LootBox } from "./loot-box/loot-box"
---
+++ src/components/loot-box/loot-box.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./loot-box.module.css";
+
+const LootBox = () => {
+    return(
+        <div className={classes.lootbox}>
+            <img alt="" href="#"></img>
+        </div>
+    );
+};
+
+export default LootBox;
