fd780e8b86b60fb6307a0b341dcbdbec08a50829
Ferbray <yt_ferbray@mail.ru>
2023-05-04 13:27:35 +0300

    some changes

--- src/components/modal/sign-in-window.jsx
+++ src/components/modal/sign-in-window.jsx
@@ -1,13 +1,11 @@
 import React from "react"
 import classes from "./modal.module.css"
 import {ManLogoNoBg} from "../../assets/images/additional";
+import { DocumentLink } from "../сommon/button";
 
 const SignInWindow = () => {
     return(
         <div className={classes.sign_in_window}>
-            <div className={classes.logo_title}>
-                InCase
-            </div>
             <div className={classes.sign_in_window_content}>
                 <img alt="" src={ManLogoNoBg}/>
                 <form className={classes.sign_in_window_content__form}>
@@ -17,7 +15,9 @@ const SignInWindow = () => {
                     <input className={classes.input_form} placeholder="Пароль" type="password"/>
                     <div className={classes.checkbox_section}>
                         <input type="checkbox" value="Admin" name="agree-eta" className={classes.checkbox_form}/>
-                        <label htmlFor="agree-eta">Пошел ты нахуй, козел</label>
+                        <label className={classes.agree_eta} htmlFor="agree-eta">
+                            {<DocumentLink text="Я согласен с условиями пользовательского соглашения" link="/info/user-agreement"/>}
+                        </label>
                     </div>
                     <button className={classes.btn_main}>
                         <div>Отправить</div>
