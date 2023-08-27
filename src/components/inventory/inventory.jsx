fdf7b0e5be9df397d56ab35021db9402ef41ebc1
GIS <yt_ferbray@mail.ru>
2023-08-27 13:18:16 +0500

    do slider

--- src/components/inventory/inventory.jsx
+++ src/components/inventory/inventory.jsx
@@ -1,44 +1,67 @@
 import React, { useEffect, useState } from 'react'
 import { Item as ItemApi } from "../../services/api"
 import User from '../../services/api/user'
+import { CounterSlider } from '../slider'
 import classes from "./inventory.module.css"
 import Item from "./item"
 
 const Inventory = (props) => {
 		const [isStart, setIsStart] = useState(true);
-		const [inventories, setInventories] = useState([]);
+		const [inventoryFirst, setInventoryFirst] = useState([]);
+		const [inventory, setInventory] = useState([]);
+		const [pages, setPages] = useState(1);
+		const [page, setPage] = useState(1);
 		
+		//TODO добавить круглишек загрузки и вытягивать расширеную информацию по страницам
+		useEffect(() => {
+				//TODO загружать новую информацию
+		}, [page, pages]);
+
 		useEffect(() => {
 				const interval = setInterval(async () => {
 						try {
+								setIsStart(false);
+
 								const userApi = new User();
 								const itemApi = new ItemApi();
-								setIsStart(false);
-								let inventory = await userApi.getInventory();
-								inventory = await itemApi.getItemsByInventory(inventory);
-								setInventories(inventory);
+
+								let result = await userApi.getInventory();
+
+								setInventoryFirst(result);
+
+								result = await itemApi.getItemsByInventory(result);
+								
+								setInventory(result);
+								setPages(Math.ceil(result.length / 20));
 						} 
 						catch (err) { }
 				}, (isStart ? 1 : 10000));
 
 				return () => clearInterval(interval);
-		}, [isStart, inventories]);
+		}, [isStart, inventory]);
 
 		return(
-				<div className={classes.inventory}>
-						{inventories.map((inventory) => 
-								<Item 
-									img={inventory.item.img}
-									name={inventory.item.name}
-									color={inventory.item.rarity}
-									date={inventory.date}
-									cost={inventory.cost}
-									id={inventory.id}
-									selectItems={props.selectItems}
-									setSelectItems={props.setSelectItems}
-									key={inventory.id}
-								/>
-						)}
+				<div className={classes.inventory_content}>
+						<div className={classes.inventory}>
+								{inventory.map((i) => 
+										<Item 
+											img={i.item.img}
+											name={i.item.name}
+											color={i.item.rarity}
+											date={i.date}
+											cost={i.cost}
+											id={i.id}
+											selectItems={props.selectItems}
+											setSelectItems={props.setSelectItems}
+											key={i.id}
+										/>
+								)}
+						</div>
+						<CounterSlider
+							pages={pages}
+							page={page}
+							setPage={setPage}
+						/>
 				</div>
 		);
 };
---
+++ src/components/slider/index.js
@@ -0,0 +1,3 @@
+export { default as CounterSlider } from "./counter-slider"
+export { default as MinSlider } from "./min-slider"
+
