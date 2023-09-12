451fb8f642e5a1992850180529a1f159dc85a077
GIS <yt_ferbray@mail.ru>
2023-09-12 19:43:38 +0500

    do profile

--- src/components/profile-settings/profile-setting.jsx
+++ src/components/profile-settings/profile-setting.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UserLogo } from '../../assets/images/additional'
 import { Download } from '../../assets/images/icon'
 import TokenService from "../../services/token"
+import Data from './components/data'
+import Input from './components/input'
 import classes from "./profile-setting.module.css"
 
 const ProfileSetting = (props) => {
-		const user = TokenService.getUser();
+		const [user, setUser] = useState(TokenService.getUser());
 		
 		return(
 			<div className={classes.profile_setting}>
@@ -15,9 +17,21 @@ const ProfileSetting = (props) => {
 									<img className={classes.load_icon} alt="" src={Download}></img>
 							</div>
 							<div className={classes.setting_data}>
-									<div>{user.name}</div>
-									<div>{user.email}</div>
-									<div>{user.role}</div>
+									<Data title={user.name} showButton={true}/>
+									<Input 
+										value={user.name} 
+										max={50} 
+										tittle="Логин"
+										name="user-name"
+									/>
+									<Data title={user.email} showButton={null}/>
+									<Input 
+										value={user.email} 
+										max={50} 
+										tittle="Почта"
+										name="user-email"
+									/>
+									<Data title={user.role} showButton={null}/>
 							</div>
 					</div>
 			</div>
