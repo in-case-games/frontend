22119b95e6b789d6285f0172b1e244209c1fb75f
GIS <yt_ferbray@mail.ru>
2023-09-13 10:10:36 +0500

    do

--- src/components/profile-settings/profile-setting.jsx
+++ src/components/profile-settings/profile-setting.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react'
 import { UserLogo } from '../../assets/images/additional'
 import { Download } from '../../assets/images/icon'
 import TokenService from "../../services/token"
-import Data from './components/data'
-import Input from './components/input'
+import InputData from './components/input-data'
 import classes from "./profile-setting.module.css"
 
 const ProfileSetting = (props) => {
@@ -17,21 +16,21 @@ const ProfileSetting = (props) => {
 									<img className={classes.load_icon} alt="" src={Download}></img>
 							</div>
 							<div className={classes.setting_data}>
-									<Data title={user.name} showButton={true}/>
-									<Input 
-										value={user.name} 
-										max={50} 
+									<InputData
+										value={user.name}
+										max={50}
 										tittle="Логин"
 										name="user-name"
 									/>
-									<Data title={user.email} showButton={null}/>
-									<Input 
-										value={user.email} 
-										max={50} 
+									<InputData
+										value={user.email}
+										max={50}
 										tittle="Почта"
 										name="user-email"
 									/>
-									<Data title={user.role} showButton={null}/>
+									<InputData
+										value={user.role}
+									/>
 							</div>
 					</div>
 			</div>
