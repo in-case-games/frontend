e648fd38b39dbaafb2f65db07816b63b27242d85
GIS <yt_ferbray@mail.ru>
2023-12-13 19:26:20 +0500

    17 - Сделал окно с пополнением

--- src/components/windows/payment/payment.jsx
+++ src/components/windows/payment/payment.jsx
@@ -2,43 +2,78 @@ import React, { useState } from "react";
 import { InCoin, InCoinGray } from "../../../assets/images/icons";
 import { ComboBox, Input } from "../../../components/common/inputs";
 import styles from "./payment.module";
+import Constants from "../../../constants";
 
-const Payment = (props) => {
-  const [amount, setAmount] = useState(0);
+const Payment = () => {
+  const [amount, setAmount] = useState(50);
+  const [typePay, setTypePay] = useState(
+    Constants.TypePayments.find((t) => t.id === 1)
+  );
+
+  const sendToPaymentForm = () => {};
+
   return (
     <div className={styles.payment}>
       <div className={styles.payment_content}>
         <div className={styles.header}>
           <div className={styles.tittle}>Выберите тип</div>
-          <div className={styles.current_course}>
-            <div className={styles.real_valuta}>1 RUB</div>
-            <div className={styles.equally}>=</div>
-            <div className={styles.our_valuta}>
-              7 <img alt="" className={styles.image} src={InCoin} />
+          {typePay.currency && typePay.rate ? (
+            <div className={styles.current_course}>
+              <div className={styles.real_valuta}>1 {typePay.currency}</div>
+              <div className={styles.equally}>=</div>
+              <div className={styles.our_valuta}>
+                {typePay.rate}{" "}
+                <img alt="" className={styles.image} src={InCoin} />
+              </div>
             </div>
-          </div>
+          ) : null}
         </div>
         <div className={styles.content}>
-          <ComboBox name="type-pay" />
-          <Input
-            subTittle="Потратите RUB: "
-            name="real-amount"
-            value={amount}
-            setValue={(v) => {
-              if (v >= 0) setAmount(v);
-            }}
-            type="number"
+          <ComboBox
+            name="type-pay"
+            value={typePay.name}
+            values={Constants.TypePayments}
+            setValue={() => {}}
+            setIndex={(id) =>
+              setTypePay(
+                Constants.TypePayments.find((t) => t.id === Number(id))
+              )
+            }
           />
-          <Input
-            subTittle="Получите InCoin: "
-            name="our-amount"
-            placeholder={`${amount * 7} инкоинов`}
-            type="number"
-            isReadOnly={true}
-          />
+          {typePay.minAmount && typePay.maxAmount ? (
+            <Input
+              subTittle={`Потратите ${typePay.currency}: `}
+              name="real-amount"
+              value={amount}
+              setValue={(v) => {
+                if (/^\d+$/.test(v)) {
+                  let newAmount = v;
+
+                  if (v < typePay.minAmount) newAmount = typePay.minAmount;
+                  if (v > typePay.maxAmount) newAmount = typePay.maxAmount;
+
+                  setAmount(newAmount);
+                }
+              }}
+              type="number"
+            />
+          ) : null}
+          {typePay.rate ? (
+            <Input
+              subTittle="Получите InCoin: "
+              name="our-amount"
+              placeholder={`${amount * typePay.rate} инкоинов`}
+              type="number"
+              isReadOnly={true}
+            />
+          ) : null}
         </div>
         <div className={styles.footer}>
-          <div className={styles.button_pay}>
+          <div className={styles.delimiter}></div>
+          {typePay.description ? (
+            <div className={styles.description}>{typePay.description}</div>
+          ) : null}
+          <div className={styles.button_pay} onClick={sendToPaymentForm}>
             {" "}
             <img alt="" src={InCoinGray} className={styles.image} />
             <div className={styles.text}>Пополнить</div>
