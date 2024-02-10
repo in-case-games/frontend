bd2fe4139afef3cc5784b225aea9f15e8f1a60f1
GIS <yt_ferbray@mail.ru>
2024-02-10 14:50:59 +0500

    22 - [NEWVERSION] v1.0.1 (Добавлена оплата)

--- src/services/notification.js
+++ src/services/notification.js
@@ -10,6 +10,16 @@ const popNotifies = () => {
 };
 
 const pushNotify = (notify) => {
+<<<<<<< Updated upstream
+=======
+  var content = JSON.stringify(notify.content);
+
+  console.log(content);
+  if (content.length > 60 || content.includes(":")) {
+    console.log(notify.content);
+    notify.content = "Содержание в консоли";
+  }
+>>>>>>> Stashed changes
   var notifications = JSON.parse(localStorage.getItem("notifications"));
 
   if (!notifications || notifications.length > 0) {
